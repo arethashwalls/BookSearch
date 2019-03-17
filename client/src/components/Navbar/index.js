3e8f6a037ef20b7c8f5f780ccaa8bdaf7ba0bb15
ASH Walls <admin@arethawalls.com>
2019-03-16 18:12:54 -0700

    I just spend almost an hour getting active tabs to style correctly -_-

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Tabs } from 'react-bulma-components/full';
 
-const Navbar = () => {
-    return <Tabs>
-        <h1>BookSearch</h1>
-        <Tabs.Tab>Search</Tabs.Tab>
-        <Tabs.Tab>Saved</Tabs.Tab>
-    </Tabs>
+class Navbar extends Component {
+    state = {
+        activePage: 'Search'
+    }
+    getNavLinkClass = path => path === this.state.activePage ? 'is-active' : '';
+    render() {
+        return <Tabs className='is-boxed'>
+            <li className={this.getNavLinkClass('Search')}>
+                <NavLink to='/search' onClick={() => this.setState({activePage: 'Search'})}>Search</NavLink>
+            </li>
+            <li className={this.getNavLinkClass("Saved")}>
+                <NavLink to='/saved' onClick={() => this.setState({activePage: 'Saved'})} >Saved Books</NavLink>
+            </li>
+        </Tabs>
+    }
 }
 
 export default Navbar;
